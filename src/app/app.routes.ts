c873af4a5335f17dcba43284e053c9b985c96c9a
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-11-05 22:01:05 -0500

    Move login route from root to 'login' and add signup route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Signup } from './pages/signup/signup';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'login',
     component: Login,
     title: 'Login'
+  },
+  {
+    path: 'signup',
+    component: Signup,
+    title: 'Signup'
   }
 ];
