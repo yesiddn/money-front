f33f7d8f1cf34891bcb685e80e48508e0590941a
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-11-15 18:51:29 -0500

    Add header component with navbar integration

---
+++ src/app/shared/components/header/header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Navbar } from "../navbar/navbar";
+
+@Component({
+  selector: 'app-header',
+  imports: [Navbar],
+  templateUrl: './header.html',
+})
+export class Header {
+
+}
