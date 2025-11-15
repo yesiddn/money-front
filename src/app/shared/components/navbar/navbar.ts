cbba1b65a4e49ebc62b0cd343760aa86409d6e4a
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-11-15 18:51:12 -0500

    Add navbar component with routing and menu selection

---
+++ src/app/shared/components/navbar/navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink],
+  templateUrl: './navbar.html',
+})
+export class Navbar {
+  menuSelected = 'dashboard';
+}
