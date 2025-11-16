fe94aed7d7224b781ff2449e9bed2d6f359f8f3e
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-11-15 21:51:54 -0500

    Add routing for layout and records components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Signup } from './pages/signup/signup';
+import { Records } from './pages/records/records';
+import { Layout } from './shared/components/layout/layout';
 
 export const routes: Routes = [
   {
@@ -12,5 +14,16 @@ export const routes: Routes = [
     path: 'signup',
     component: Signup,
     title: 'Signup'
+  },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        component: Records,
+        title: 'Historia de transacciones'
+      }
+    ]
   }
 ];
