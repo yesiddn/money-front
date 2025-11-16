c284308662e42200565862a24ef64cdf0df9afbb
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-11-15 21:51:41 -0500

    Add record-filters component with HTML template

---
+++ src/app/modules/components/record-filters/record-filters.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-record-filters',
+  imports: [],
+  templateUrl: './record-filters.html',
+})
+export class RecordFilters {
+
+}
