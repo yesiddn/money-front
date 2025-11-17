f08645f3f9cf7fe1eddad445ca8612a1d6ae35b3
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-11-17 15:34:41 -0500

    Add token management methods for access and refresh tokens

--- src/app/services/token.ts
+++ src/app/services/token.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { setCookie, getCookie, removeCookie } from 'typescript-cookie'
+import { jwtDecode, JwtPayload } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,54 @@ export class Token {
 
   removeAccessToken(): void {
     removeCookie('access_token', { path: '/' });
+  }
+
+  isValidAccessToken(): boolean {
+    const token = this.getAccessToken();
+    if (!token) {
+      return false;
+    }
+
+    const decodeToken = jwtDecode<JwtPayload>(token);
+    if (decodeToken && decodeToken?.exp) {
+      const tokenDate = new Date(0);
+      tokenDate.setUTCSeconds(decodeToken.exp);
+
+      const today = new Date();
+      return tokenDate.getTime() > today.getTime();
+    }
+
+    return false;
+  }
+
+  // Refresh Token Methods
+  saveRefreshToken(token: string): void {
+    setCookie('refresh_token', token, { expires: 365, path: '/' });
+  }
+
+  getRefreshToken(): string | undefined {
+    return getCookie('refresh_token');
+  }
+
+  removeRefreshToken(): void {
+    removeCookie('refresh_token', { path: '/' });
+  }
+
+  isValidRefreshToken(): boolean {
+    const refreshtoken = this.getRefreshToken();
+    if (!refreshtoken) {
+      return false;
+    }
+
+    const decodeToken = jwtDecode<JwtPayload>(refreshtoken);
+    if (decodeToken && decodeToken?.exp) {
+      const tokenDate = new Date(0);
+      tokenDate.setUTCSeconds(decodeToken.exp);
+
+      const today = new Date();
+      return tokenDate.getTime() > today.getTime();
+    }
+
+    return false;
   }
 }
