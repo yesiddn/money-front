d4c5461dca621266e781c0be9242ddc1cf1a5f31
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-11-25 20:30:41 -0500

    Add p-toast component and update layout for improved notifications

--- src/app/shared/components/layout/layout.ts
+++ src/app/shared/components/layout/layout.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastModule } from 'primeng/toast';
 import { Header } from '../header/header';
 import { Navbar } from "../navbar/navbar";
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, Header, Navbar],
+  imports: [RouterOutlet, Header, Navbar, ToastModule],
   templateUrl: './layout.html',
+  providers: [MessageService],
 })
 export class Layout {
 
