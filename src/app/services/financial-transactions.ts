0849f2228bc986efc62cb3d95f981175dfa67257
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-11-25 20:30:22 -0500

    Add FinancialTransactions service for fetching records with token validation

---
+++ src/app/services/financial-transactions.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { checkToken } from '../interceptors/token-interceptor';
+import { Record } from '../pages/records/record.intereface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FinancialTransactions {
+  http = inject(HttpClient);
+  apiURL = environment.API_URL;
+
+  getRecords() {
+    return this.http.get<Record[]>(`${this.apiURL}/api/records/`, { context: checkToken() });
+  }
+}
