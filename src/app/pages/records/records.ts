1aa1023b8af89e2766334a6ee6b848fae95e6ab9
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-11-25 20:30:32 -0500

    Refactor records component to enhance transaction display and error handling

--- src/app/pages/records/records.ts
+++ src/app/pages/records/records.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
-import { Navbar } from '../../shared/components/navbar/navbar';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RecordFilters } from "../../modules/components/record-filters/record-filters";
+import { FinancialTransactions } from '../../services/financial-transactions';
+import { Record } from './record.intereface';
+import { CurrencyPipe, DecimalPipe } from '@angular/common';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-records',
-  imports: [RecordFilters],
+  imports: [RecordFilters, CurrencyPipe, DecimalPipe],
   templateUrl: './records.html',
 })
-export class Records {
+export class Records implements OnInit {
+  financialTransactions = inject(FinancialTransactions);
+  messageService = inject(MessageService);
 
+  transactions = signal<Record[]>([]);
+
+  ngOnInit() {
+    // this.messageService.add({severity:'error', summary: 'Error', detail: 'No se pudieron cargar las transacciones.'});
+    this.financialTransactions.getRecords()
+      .subscribe({
+        next: (data: Record[]) => {
+          this.transactions.set(data);
+        },
+        error: (error) => {
+          console.error('Error fetching records:', error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'No se pudieron cargar las transacciones.',
+          });
+        }
+      });
+  }
+
+  getSpanClasses(typeRecord: 'expense' | 'income' | 'transfer' | 'investment'): string {
+    switch (typeRecord) {
+      case 'expense':
+      case 'investment':
+        return 'text-red-600 bg-red-500/20';
+      case 'income':
+        return 'text-green-600 bg-green-500/20';
+      case 'transfer':
+        return 'text-primary-600 bg-primary-500/20';
+      default:
+        return '';
+    }
+  }
 }
