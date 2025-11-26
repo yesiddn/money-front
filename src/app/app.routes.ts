c41839a3d11382e4e4980e667b29a5b461af6b9c
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-11-25 20:30:50 -0500

    Update routing to include guards for login, signup, and layout components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,26 +3,31 @@ import { Login } from './pages/login/login';
 import { Signup } from './pages/signup/signup';
 import { Records } from './pages/records/records';
 import { Layout } from './shared/components/layout/layout';
+import { authGuard } from './guards/auth-guard';
+import { redirectGuard } from './guards/redirect-guard';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: Login,
+    canActivate: [redirectGuard],
     title: 'Login'
   },
   {
     path: 'signup',
     component: Signup,
+    canActivate: [redirectGuard],
     title: 'Signup'
   },
   {
     path: '',
     component: Layout,
+    canActivate: [authGuard],
     children: [
       {
         path: '',
         component: Records,
-        title: 'Historia de transacciones'
+        title: 'Historial de transacciones'
       }
     ]
   }
