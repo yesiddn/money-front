f46f8a8b7fc7e8f9a5e3fe5e0b2504bb1bc3b369
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-11-25 20:28:20 -0500

    Add interfaces for Account, Category, and Record to define data structures

---
+++ src/app/pages/records/record.intereface.ts
@@ -0,0 +1,30 @@
+export interface Account {
+  id: number;
+  name: string;
+  description: string;
+  currency: string;
+  created_at: string;
+  updated_at: string;
+  user: string;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+  description: string;
+}
+
+export interface Record {
+  id: number;
+  title: string;
+  description: string;
+  amount: string;
+  account: Account;
+  typeRecord: 'expense' | 'income' | 'transfer' | 'investment';
+  category: Category;
+  paymentType: 'cash' | 'transfer' | 'card';
+  currency: string;
+  date_time: string;
+  created_at: string;
+  updated_at: string;
+}
