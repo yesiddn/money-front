c89912e1bc8a933780cd5850b188a383e0d87720
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-12-24 22:46:45 -0500

    Add visibility management for edit record dialog in ManageRecordsDialogs

--- src/app/transactions/manage-records/manage-records-dialogs.ts
+++ src/app/transactions/manage-records/manage-records-dialogs.ts
@@ -5,8 +5,10 @@ import { Injectable, signal } from '@angular/core';
 })
 export class ManageRecordsDialogs {
   private createRecordDialogVisible = signal(false);
+  private editRecordDialogVisible = signal(false);
 
   createRecordDialogVisibleState = this.createRecordDialogVisible.asReadonly();
+  editRecordDialogVisibleState = this.editRecordDialogVisible.asReadonly();
 
   showCreateRecordDialog() {
     this.createRecordDialogVisible.set(true);
@@ -14,5 +16,13 @@ export class ManageRecordsDialogs {
 
   hideCreateRecordDialog() {
     this.createRecordDialogVisible.set(false);
+  }
+
+  showEditRecordDialog() {
+    this.editRecordDialogVisible.set(true);
+  }
+
+  hideEditRecordDialog() {
+    this.editRecordDialogVisible.set(false);
   }
 }
