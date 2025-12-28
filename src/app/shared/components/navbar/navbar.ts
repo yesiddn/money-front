e7ac4e6b8d5e43c428dd65669237c30deeb54f27
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-12-27 23:25:52 -0500

    Refactor navbar dialog handling to unify create dialog methods

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { ManageRecordsDialogs } from '@app/transactions/manage-records/manage-records-dialogs';
 import { NavbarService } from './navbar-service';
+import { ManageDialogs } from '@app/shared/dialogs/manage-dialogs';
 
 @Component({
   selector: 'app-navbar',
@@ -11,10 +12,16 @@ import { NavbarService } from './navbar-service';
 export class Navbar {
   private navbarService = inject(NavbarService);
   private manageRecordsDialogs = inject(ManageRecordsDialogs);
+  private manageDialogs = inject(ManageDialogs);
 
   menuSelected = this.navbarService.currentMenu;
 
-  showCreateRecordDialog() {
+  showCreateDialog() {
+    if (this.menuSelected() === 'accounts') {
+      this.manageDialogs.showCreateAccountDialog();
+      return;
+    }
+
     this.manageRecordsDialogs.showCreateRecordDialog();
   }
 }
