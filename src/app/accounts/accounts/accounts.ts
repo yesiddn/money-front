bfc92596aa537c6a7382fe132dcff9c83cd80e9f
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-12-27 23:26:44 -0500

    Refactor accounts component to integrate CreateAccounts and EditAccounts, enhancing dialog management and account handling

--- src/app/accounts/accounts/accounts.ts
+++ src/app/accounts/accounts/accounts.ts
@@ -1,19 +1,23 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { NavbarService } from '@app/shared/components/navbar/navbar-service';
 import { ManageAccounts } from '../manage-accounts';
-import { Account } from '@app/models/record.intereface';
 import { AccountIcon } from '../account-icon/account-icon';
 import { DecimalPipe } from '@angular/common';
+import { CreateAccounts } from '../manage-accounts/create-accounts/create-accounts';
+import { Account } from '@app/models/account.interface';
+import { ManageDialogs } from '@app/shared/dialogs/manage-dialogs';
+import { EditAccounts } from '../manage-accounts/edit-accounts/edit-accounts';
 
 @Component({
   selector: 'app-accounts',
-  imports: [AccountIcon, DecimalPipe],
+  imports: [AccountIcon, DecimalPipe, CreateAccounts, EditAccounts],
   templateUrl: './accounts.html',
   styleUrl: './accounts.css',
 })
 export class Accounts implements OnInit {
   private navbarService = inject(NavbarService);
   private manageAccounts = inject(ManageAccounts);
+  private manageDialogs = inject(ManageDialogs);
   private images = [
     './images/clouds.svg',
     './images/adventure.svg',
@@ -28,6 +32,7 @@ export class Accounts implements OnInit {
   ];
 
   accounts = signal<Account[]>([]);
+  accountToEdit = signal<Account | null>(null);
 
   ngOnInit() {
     this.navbarService.setCurrentMenu('accounts');
@@ -48,8 +53,18 @@ export class Accounts implements OnInit {
   }
 
   openEditAccountDialog(account: Account) {
-    console.log('Editing account:', account);
-    // this.manageAccounts.openEditAccountDialog(account);
+    this.accountToEdit.set(account);
+    this.manageDialogs.showEditAccountDialog();
+  }
+
+  handleNewAccount(newAccount: Account) {
+    this.accounts.update(accounts => [...accounts, newAccount]);
+  }
+
+  handleUpdatedAccount(updatedAccount: Account) {
+    this.accounts.update(accounts => accounts.map(account =>
+      account.id === updatedAccount.id ? updatedAccount : account
+    ));
   }
 
   getImageUrl(account: Account): string {
