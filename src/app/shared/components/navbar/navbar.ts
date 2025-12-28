d1596e32c6d415b768fdd5695ad2284d5d17dbd9
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-12-27 19:40:16 -0500

    Refactor navbar component to use menuSelected method for dynamic class binding

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { ManageRecordsDialogs } from '@app/transactions/manage-records/manage-records-dialogs';
+import { NavbarService } from './navbar-service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +9,10 @@ import { ManageRecordsDialogs } from '@app/transactions/manage-records/manage-re
   templateUrl: './navbar.html',
 })
 export class Navbar {
+  private navbarService = inject(NavbarService);
   private manageRecordsDialogs = inject(ManageRecordsDialogs);
 
-  menuSelected = 'dashboard';
+  menuSelected = this.navbarService.currentMenu;
 
   showCreateRecordDialog() {
     this.manageRecordsDialogs.showCreateRecordDialog();
